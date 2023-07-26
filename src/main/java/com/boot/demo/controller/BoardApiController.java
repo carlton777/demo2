df5093b680869932775f951c10c55b21957d957f
carlton777 <sangkoo@naver.com>
2023-07-26 23:49:47 +0900

    update

---
+++ src/main/java/com/boot/demo/controller/BoardApiController.java
@@ -0,0 +1,55 @@
+package com.boot.demo.controller;
+
+import com.boot.demo.model.Board;
+import com.boot.demo.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+  @Autowired
+  private BoardRepository repository;
+
+  @GetMapping("/boards")
+  List<Board> all() {
+    return repository.findAll();
+  }
+  // end::get-aggregate-root[]
+
+  @PostMapping("/boards")
+  Board newBoard(@RequestBody Board newBoard) {
+    return repository.save(newBoard);
+  }
+
+  // Single item
+
+  @GetMapping("/boards/{id}")
+  Board one(@PathVariable Long id) {
+
+    return repository.findById(id).orElse(null);
+  }
+
+  @PutMapping("/boards/{id}")
+  Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+    return repository.findById(id)
+        .map(board -> {
+          board.setTitle(newBoard.getTitle());
+          board.setContent(newBoard.getContent());
+          return repository.save(board);
+        })
+        .orElseGet(() -> {
+          newBoard.setId(id);
+          return repository.save(newBoard);
+        });
+  }
+
+  @DeleteMapping("/boards/{id}")
+  void deleteBoard(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
+}
--- src/main/java/com/boot/demo/controller/BoardController.java
+++ src/main/java/com/boot/demo/controller/BoardController.java
@@ -31,7 +31,9 @@ public class BoardController {
         return "board/list";
     }
     @GetMapping("/form")
-    public String form(Model model, @RequestParam Long id){
+    public String form(Model model, @RequestParam(required = false) Long id){
+        log.info("@# id"+id);
+
         if (id == null){
             model.addAttribute("board", new Board());
         }else {
